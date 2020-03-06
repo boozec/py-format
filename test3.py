750c175f317f55f84bed7b94123bb5045eecf82e
Santo Cariotti <dcariotti24@gmail.com>
2020-03-06 12:39:31 +0000

    black format

--- test3.py
+++ test3.py
@@ -5,14 +5,10 @@ from functools import wraps
 from datetime import datetime
 
 
-def send_email(sender, 
-               
-               email, 
-               activation_code, title,
-               template):
+def send_email(sender, email, activation_code, title, template):
     msg = Message(title, sender=sender, recipients=[email])
 
-    rest_link = 'http://localhost:8080/app'
+    rest_link = "http://localhost:8080/app"
     msg.html = render_template(template, link=rest_link, code=activation_code)
     mail.send(msg)
 
